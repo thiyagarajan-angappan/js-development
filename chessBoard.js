e562ca376f10f83cf3ac8fc39bc194a20c906077
tj <tj.1283.us@gmail.com>
2018-07-02 16:26:47 -0500

    chessboard js with test html

---
+++ chessBoard.js
@@ -0,0 +1,37 @@
+// chess board
+
+// 
+let gridSize = prompt('Enter grid size'),
+    black = prompt('Enter character for black'),
+    white = prompt('Enter character for white');
+
+const printBoard = function(gridSize = 8, black = '#', white = ' '){
+    
+        let totalSquares = gridSize*gridSize,
+            prevChar = '', currentChar = '', line = '';
+    
+        for (let length=1;length<=totalSquares;length++){
+        if (prevChar == white){
+            currentChar = black;
+        }
+        else{
+            currentChar = white;
+        }
+
+        line = line + currentChar;
+
+        if(length%gridSize != 0){
+            prevChar = currentChar;
+        }
+        else{
+            line = line + '\n';
+            if(gridSize%2 != 0){
+                prevChar = currentChar;
+            }
+        }
+
+    }
+    return line;
+};
+
+console.log(printBoard(gridSize,black));
