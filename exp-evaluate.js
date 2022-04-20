58695e7e8924f5fc0230d8a4d78c3b93c9afc628
thiyagarajan-angappan <thiyagarajan.angappan@gmail.com>
2022-04-20 17:39:49 -0500

    update solution for expression evaluator

--- exp-evaluate.js
+++ exp-evaluate.js
@@ -1,1 +1,21 @@
-//provided a string like "5+3*2", evaluate the expression and return the result
+// provided a string like "5+3*2", evaluate the expression and return the result
+// string will only contain addition and multiplication operators
+// string will always have a valid expression (x+y or x*y) or a valid number(33 or 42)
+
+const evaluateExp = function(expStr){
+    if (expStr.includes('+') && expStr.includes('*')){
+	let addArr = expStr.split('+').map( exp => {
+	    return exp.includes('*') ? exp.split('*').reduce((a,b) => a*b) : +exp;
+	});
+	return addArr.reduce((a,b) => a+b);
+    } else if (expStr.includes('*')){
+	return expStr.split('*').reduce((a,b) => a*b);
+    } else if (expStr.includes('+')){
+	return expStr.split('+').reduce((a,b) => parseInt(a)+parseInt(b));
+    } else {
+	return expStr;
+    }
+}
+
+const expression = '6+3*12';
+console.log(evaluateExp(expression));
